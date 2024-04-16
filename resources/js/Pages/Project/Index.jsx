0ad6e19bc1c1e8fd26520be006281f724d2231e9
André Ferreira <110822583+andre9277@users.noreply.github.com>
2024-04-16 21:22:30 +0100

    SelectInput component
    SelectInout component created and added to Project page. The user can select the status of the project witha  select box or text box.

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,4 +1,6 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import {
   PROJECT_STATUS_CLASS_MAP,
@@ -6,7 +8,28 @@ import {
 } from "@/constants.jsx";
 import { Head, Link } from "@inertiajs/react";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+  //My queryParams is going to be an object
+  queryParams = queryParams || {};
+
+  //Function for search box
+  const searchFieldChanged = (name, value) => {
+    if (value) {
+      queryParams[name] = value;
+    } else {
+      delete queryParams[name];
+    }
+
+    router.get(route("project.index"), queryParams);
+  };
+
+  //Check if user clicks the enter key, search
+  const onKeyPress = (name, e) => {
+    if (e.key !== "Enter") return;
+
+    searchFieldChanged(name, e.target.value);
+  };
+
   return (
     <Authenticated
       user={auth.user}
@@ -32,6 +55,41 @@ export default function Index({ auth, projects }) {
                     <th className="px-3 py-2">Due Date</th>
                     <th className="px-3 py-3">Created By</th>
                     <th className="px-3 py-3 text-right">Actions</th>
+                  </tr>
+                </thead>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput
+                        className="w-full"
+                        defaultValue={queryParams.name}
+                        placeholder="Project Name"
+                        onBlur={(e) =>
+                          searchFieldChanged("name", e.target.value)
+                        }
+                        onKeyPress={(e) => onKeyPress("name", e)}
+                      />
+                    </th>
+                    <th className="px-3 py-3">
+                      <SelectInput
+                        className="w-full"
+                        defaultValue={queryParams.status}
+                        onChange={(e) =>
+                          searchFieldChanged("status", e.target.value)
+                        }
+                      >
+                        <option value="">Select Status</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </SelectInput>
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
                   </tr>
                 </thead>
 
